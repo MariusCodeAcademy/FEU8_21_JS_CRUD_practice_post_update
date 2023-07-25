f1d8eb6982d6f4de00e0eb051cc821743536eb5c
Marius CA <mariuskcodeacademy@gmail.com>
2023-07-25 11:14:13 +0300

    newPostHandler

--- js/add-post.js
+++ js/add-post.js
@@ -1,1 +1,25 @@
 console.log('add-post.js file was loaded');
+
+// taikomes
+const els = {
+  form: document.forms[0],
+  title: document.getElementById('title'),
+  body: document.getElementById('body'),
+  tags: document.getElementById('tags'),
+};
+console.log('els ===', els);
+
+// uzdeti formai submit listeneri
+els.form.addEventListener('submit', newPostHandler);
+
+// formos apdorojimo funkcija
+/**
+ * Pagrindine formo apdorojimo funkcija
+ * @param {SubmitEvent} event
+ */
+function newPostHandler(event) {
+  // stabdom perkrovima
+  event.preventDefault();
+  console.log('submiting form');
+  // paimti visus inputus is formos is sudeti i viena objekta
+}
