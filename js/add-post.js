7e6590acdcc4f4c59c2f860f173751e9863591c4
Marius CA <mariuskcodeacademy@gmail.com>
2023-07-25 11:55:37 +0300

    sendNewPostFetch

--- js/add-post.js
+++ js/add-post.js
@@ -22,4 +22,38 @@ function newPostHandler(event) {
   event.preventDefault();
   console.log('submiting form');
   // paimti visus inputus is formos is sudeti i viena objekta
+  const newPostObj = {
+    title: els.title.value.trim(),
+    body: els.body.value.trim(),
+    tags: els.tags.value.trim(),
+    userId: 5,
+  };
+  // newPostObj.tags - turi buti masyvas
+  console.log('newPostObj ===', newPostObj);
+  sendNewPostFetch(newPostObj);
+}
+
+function sendNewPostFetch(newPostDataObj) {
+  console.log('sendNewPostFetch fn ran ===', newPostDataObj);
+  // verciam js objekta i json
+  const newPostInJson = JSON.stringify(newPostDataObj);
+  fetch('https://dummyjson.com/posts/add', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: newPostInJson,
+  })
+    .then((resp) => resp.json())
+    .then((postRezult) => {
+      console.log('postRezult ===', postRezult);
+      if (postRezult.id) {
+        // jei gryzo id tai sukurtas irasas
+        console.log('sekme');
+        // naviguoti i index.html
+        // window.location.href = 'index.html';
+      } else {
+        // kitu atveju klaida
+        console.warn('kazkas nepavyko', postRezult.message);
+      }
+    })
+    .catch(console.warn);
 }
