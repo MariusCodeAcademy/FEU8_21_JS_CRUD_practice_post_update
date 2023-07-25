01dda88e7fe137804ead257583e6b6729a54e662
Marius CA <mariuskcodeacademy@gmail.com>
2023-07-25 10:51:16 +0300

    dynamic single post

--- js/single-post.js
+++ js/single-post.js
@@ -1,7 +1,8 @@
-'use strict';
+import { crEl } from './helper/crEl.js';
 console.log('single-post.js file was loaded');
 
 const url = 'https://dummyjson.com/posts';
+const contentEl = document.getElementById('content');
 
 // gauti pId is url parametru
 
@@ -16,8 +17,45 @@ function getPostById() {
     .then((resp) => resp.json())
     .then((postObj) => {
       console.log('postObj ===', postObj);
+      renderPost(postObj);
     })
     .catch(console.warn);
 }
 getPostById();
 // sugeneruoti html
+
+/*
+{
+  id: 1,
+  title: 'His mother had always taught him',
+  body: "His mother ",
+  userId: 9,
+  tags: ['history', 'american', 'crime'],
+  reactions: 2,
+}
+
+<h1 class='main-title'>title</h1>
+<p class="text">body</p>
+<ul class="unlisted">
+  <li class="tag">tag</li>
+</ul>
+<p class="react">2 People liked the post</p>
+*/
+function renderPost(pObj) {
+  console.log('renderPost ran ', pObj);
+  const title = crEl('h1', { class: 'main-title' }, pObj.title);
+  const pEl = crEl('p', { class: 'text' }, pObj.body);
+  const tagTitle = crEl('h2', { class: 'tag-title' }, 'Tags:');
+
+  const ulEl = crEl('ul', { class: 'unlisted flex tags' });
+  pObj.tags.forEach((tagString) => {
+    const liEl = crEl('li', { class: 'tag' }, tagString);
+    ulEl.append(liEl);
+  });
+  const pLikes = crEl(
+    'p',
+    { class: 'react' },
+    `${pObj.reactions} People liked the post`
+  );
+  contentEl.append(title, pEl, tagTitle, ulEl, pLikes);
+}
