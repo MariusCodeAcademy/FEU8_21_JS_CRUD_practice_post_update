bd2eff1dcad8b050bd9fc5478d8d49905d8ac392
Marius CA <mariuskcodeacademy@gmail.com>
2023-07-26 10:59:23 +0300

    editr in progrress

--- js/edit-post.js
+++ js/edit-post.js
@@ -1,1 +1,20 @@
 console.log('edit-post.js file was loaded');
+
+// gauti id parametra kurio post informacijos reikia
+const params = new URLSearchParams(window.location.search);
+const postId = params.get('postId');
+console.log('postId ===', postId);
+
+// parsiusti to post objekta
+function getSinglePost() {
+  fetch();
+  // iskonsolinti gauta post objekta
+}
+
+// paimti is objekto reiksmes ir supildyti formos inputus
+
+// stebeti formos pateikima
+
+// pateikiant forma surininkti inputus i objekta
+
+// siusti patch metodu atnaujinimus
