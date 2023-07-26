6088e653ccb7425d6d00e05da668a503bec6b2b8
Marius CA <mariuskcodeacademy@gmail.com>
2023-07-26 10:42:29 +0300

    deletePostFetch confirm

--- js/single-post.js
+++ js/single-post.js
@@ -4,6 +4,7 @@ import { url } from './helper/config.js';
 console.log('single-post.js file was loaded');
 
 const contentEl = document.getElementById('content');
+const deleteBtn = document.getElementById('delete');
 
 // gauti pId is url parametru
 
@@ -11,13 +12,22 @@ const params = new URLSearchParams(window.location.search);
 const postId = params.get('pId');
 console.log('postId ===', postId);
 
+deleteBtn.addEventListener('click', () => {
+  // gauti console log kai paspaudziam delete mygtuka
+  // isspausdinti posto id paspaudus delete mygtuka
+  console.log('delete was pressed', postId);
+  if (confirm('ar tikrai istrinti?')) {
+    deletePostFetch(postId);
+  }
+});
+
 // parsiusti to posto objekta
 
 function getPostById() {
   fetch(`${url}/${postId}`)
     .then((resp) => resp.json())
     .then((postObj) => {
-      console.log('postObj ===', postObj);
+      // console.log('postObj ===', postObj);
       renderPost(postObj);
     })
     .catch(console.warn);
@@ -43,7 +53,7 @@ getPostById();
 <p class="react">2 People liked the post</p>
 */
 function renderPost(pObj) {
-  console.log('renderPost ran ', pObj);
+  // console.log('renderPost ran ', pObj);
   const title = crEl('h1', { class: 'main-title' }, pObj.title);
   const pEl = crEl('p', { class: 'text' }, pObj.body);
   const tagTitle = crEl('h2', { class: 'tag-title' }, 'Tags:');
@@ -57,5 +67,22 @@ function renderPost(pObj) {
   contentEl.append(title, pEl, tagTitle, ulEl, pLikes);
 }
 
-// gauti console log kai paspaudziam delete mygtuka
-// isspausdinti posto id paspaudus delete mygtuka
+// delete post from db
+function deletePostFetch(idToDelete) {
+  console.log('deletePostFetch');
+  console.log('idToDelete ===', idToDelete);
+  fetch(`${url}/${idToDelete}`, {
+    method: 'DELETE',
+  })
+    .then((resp) => {
+      console.log('resp ===', resp);
+      if (resp.ok === true) {
+        // pavyko istrinti
+        // nunaviguoti i indes.html
+        window.location.href = 'index.html';
+      } else {
+        // nepavyko
+      }
+    })
+    .catch((err) => console.warn(err));
+}
